3b78fadcd3911b6d44234437bb1d4ccaf2ffca75
Lucas <li,lucasinocencio@gmail.com>
2025-05-18 16:53:21 -0300

    Refactor HTML structure and improve selectors for card filtering
    - Updated index.html to streamline the card selection options by changing option values to names for better readability.
    - Removed unnecessary comments and cleaned up the HTML structure.
    - Enhanced the search functionality in script.js to filter cards based on multiple selectors and search input.
    - Added a new code block in main.ipynb to extract text from images and update card descriptions.
    - Removed main.py as its functionality has been integrated into the Jupyter notebook.
    - Updated styles.css to remove redundant styles.

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-// Display the results
-fetch('./cards.json')
+function search() {
+    fetch('./cards.json')
     .then((response) => response.json())
     .then((json) => {
         const container = document.getElementById('cardResults');
+        container.innerHTML = ''; // Clear previous results
+        // filter cards by multiple selectors
+        const filters = [
+            { id: 'GuildSelector', key: 'guild' },
+            { id: 'ClassSelector', key: 'class' },
+            { id: 'RaceSelector', key: 'race' },
+            { id: 'LevelSelector', key: 'level' },
+            { id: 'RaritySelector', key: 'rarity' },
+        ];
+        filters.forEach(({ id, key }) => {
+            const selector = document.getElementById(id);
+            const value = selector.options[selector.selectedIndex].value;
+            if (value !== 'All') {
+            json = json.filter(card => card[key] == value);
+            }
+        });
+
+        // order cards by
+        const orderBy = document.getElementById('OrderBy');
+        const orderValue = orderBy.options[orderBy.selectedIndex].value;
+
+        // filter cards by description
+        const searchValue = document.getElementById('SearchBar').value.toLowerCase();
+        if (searchValue) {
+            json = json.filter((card) => card.description.toLowerCase().includes(searchValue));
+        }
+
         json.forEach((card) => {
             const cardElement = document.createElement('div');
             cardElement.className = 'card';
@@ -15,4 +42,5 @@ fetch('./cards.json')
             `;
             container.appendChild(cardElement);
         });
-    })
+    });
+}
