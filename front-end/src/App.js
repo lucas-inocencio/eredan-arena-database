e31d857ea5a3515e86b247dbd02b2e767c4dff7d
Lucas <li,lucasinocencio@gmail.com>
2025-05-25 20:42:12 -0300

    feat: Replace EredanArena component with EACards; remove unused EredanArena file

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import EredanArena from './EredanArena';
+import EACards from './EACards';
 import './styles.css';
 
 function App() {
   return (
     <div className="App">
-      <EredanArena />
+      <EACards />
     </div>
   );
 }
